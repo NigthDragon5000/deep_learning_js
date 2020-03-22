95f808f06c110665234d9e090793a8b8e137d549
Jair Condori <jjcc5000@gmail.com>
2020-03-21 19:39:37 -0500

    adicionales

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ let app = express();
 
 app.use(express.static("./static"));
 
-app.listen(3000, function() {
-    console.log("Listening on port 81");
+app.listen(8080, function() {
+    console.log("Listening on port 8080");
 });
 
 //https://teachablemachine.withgoogle.com/models/YZIWGg6E/
